5658bc577c8f9d337b7eac0ef64ed4595baefd1b
Travel1999 <bobrovdmitry01@mail.ru>
2023-10-27 12:35:02 +0300

    some changes

--- src/app.js
+++ src/app.js
@@ -13,41 +13,41 @@ const server = http.createServer(async (request, response) => {
 		response.write(getUsers());
 		response.end();
 		return;
-	} else if (request.url) {
-		if (request.url === `/?hello=${userName}`) {
-			if (userName) {
-				response.status = 200;
-				response.statusMessage = 'OK';
-				response.header = 'Content-Type: text/plain';
-				response.write(`Hello, ${userName}`);
-				response.end();
-				return;
-			}
-			if (userName === '') {
-				response.status = 400;
-				response.statusMessage = 'OK';
-				response.header = 'Content-Type: text/plain';
-				response.write('Enter a name');
-				response.end();
-				return;
-			}
+	} else if (request.url === `/?hello=${userName}`) {
+		if (userName) {
+			response.status = 200;
+			response.statusMessage = 'OK';
+			response.header = 'Content-Type: text/plain';
+			response.write(`Hello, ${userName}`);
+			response.end();
 			return;
 		}
+		if (userName === '') {
+			response.status = 400;
+			response.statusMessage = 'OK';
+			response.header = 'Content-Type: text/plain';
+			response.write('Enter a name');
+			response.end();
+			return;
+		}
+	} else if (request.url === '/') {
+		response.status = 200;
+		response.statusMessage = 'OK';
+		response.header = 'Content-Type: text/plain';
+		response.write('Hello, World!');
+		response.end();
+		return;
+	} else {
 		response.status = 500;
 		response.statusMessage = 'OK';
 		response.header = 'Content-Type: text/plain';
 		response.write('');
 		response.end();
 		return;
-	} else {
-		response.status = 200;
-		response.statusMessage = 'OK';
-		response.header = 'Content-Type: text/plain';
-		response.write('Hello, World!');
-		response.end();
 	}
 });
 
 server.listen(3003, () => {
 	console.log('Сервер запущен по адресу: http://127.0.0.1:3003');
+	
 });
